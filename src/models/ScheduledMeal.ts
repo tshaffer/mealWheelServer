353e65f446a34f904534a63ea797981ce82ea671
Ted Shaffer <shaffer.family@gmail.com>
2022-10-01 05:50:49 -0700

    saladId, veggieId, and sideId no longer required fields in db.

--- src/models/ScheduledMeal.ts
+++ src/models/ScheduledMeal.ts
@@ -4,14 +4,20 @@ const Schema = mongoose.Schema;
 
 mongoose.Schema.Types.String.checkRequired(v => typeof v === 'string');
 
+/*
+    saladId: { type: String, required: true },
+    veggieId: { type: String, required: true },
+    sideId: { type: String, required: true },
+*/
+
 const ScheduledMealSchema = new Schema(
   {
     id: { type: String, required: true },
     userId: { type: String, required: true },
     mainDishId: { type: String, required: true },
-    saladId: { type: String, required: true },
-    veggieId: { type: String, required: true },
-    sideId: { type: String, required: true },
+    saladId: { type: String },
+    veggieId: { type: String },
+    sideId: { type: String },
     dateScheduled: { type: Date, required: true },  // https://mongoosejs.com/docs/schematypes.html#dates
     status: { type: Number, required: true },
   },
