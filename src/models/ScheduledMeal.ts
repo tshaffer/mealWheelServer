364ed38e9b3d2dfa55b83f8f9040f82b6ee78f88
Ted Shaffer <shaffer.family@gmail.com>
2023-04-30 08:49:51 -0700

    correct property name in model.

--- src/models/ScheduledMeal.ts
+++ src/models/ScheduledMeal.ts
@@ -9,7 +9,7 @@ const ScheduledMealSchema = new Schema(
     id: { type: String, required: true },
     userId: { type: String, required: true },
     mainDishId: { type: String, required: true },
-    accompanimentDishIds: { type: [String], required: true },
+    accompanimentIds: { type: [String], required: true },
     dateScheduled: { type: Date, required: true },  // https://mongoosejs.com/docs/schematypes.html#dates
     status: { type: Number, required: true },
   },
